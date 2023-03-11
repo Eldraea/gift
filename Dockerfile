e8403183e501e80a178115c43ccf3703e504d10d
Dabi <keitadk9@gmail.com>
2023-03-11 21:00:43 +0100

    fix: CICD

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
-FROM maven:3.6.3 AS maven
+FROM maven:3.9.0 AS maven
 # Create a workdir for our app
 WORKDIR /usr/src/app
 COPY . /usr/src/app
 
 # Compile and package the application to an executable JAR
 RUN mvn clean package -DskipTests
-# Using java 11
-FROM openjdk:11-jdk
+# Using java 17
+FROM openjdk:17-alpine
 
 ARG JAR_FILE=/usr/src/app/target/*.jar
 # Copying JAR file
 COPY --from=maven ${JAR_FILE} app.jar
 
 ENTRYPOINT ["java","-jar","/app.jar"]
+EXPOSE 8080
--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,28 @@
 echo 'Starting to Deploy...'
 
 # Install required dependencies
-sudo apt-get update
-sudo apt-get upgrade
-yes | sudo apt install openjdk-11-jdk
-yes | sudo apt-get install nginx
-yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
-yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-yes | sudo apt install docker-ce
+sudo yum update
+sudo yum upgrade
+yes | sudo yum install openjdk-11-jdk
+yes | sudo yum install nginx
+yes | sudo yum install apt-transport-https ca-certificates curl software-properties-common
 
-# make sure demo docker is not running
-sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=demo:latest --format="{{.ID}}"))
+
+docker-compose down
 
 # copy nginx conf to default
-sudo cp nginx.conf /etc/nginx/conf.d/default.conf
+#sudo cp nginx.conf /etc/nginx/conf.d/default.conf
 
-sudo systemctl restart nginx
 
 # build dockerfile
-sudo docker build -f Dockerfile -t demo:latest .
+#sudo docker build -f Dockerfile -t demo:latest .
 
+# docker compose up
+docker-compose up -d --build
 # run in detached mode
-sudo docker run -p 8080:8080 -d demo:latest
+#sudo docker run -p 8080:8080 -d demo:latest
+
+
 
 sleep 15
 
